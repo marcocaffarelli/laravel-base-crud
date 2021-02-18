68f60c7092aa25645fc016a55a934355b68c7527
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-18 20:20:57 +0100

    Stampati in index i dati del database
    Stampati in index i dati del database

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,2 +1,9 @@
 <h2>Prova</h2>
-
+<div>
+    @foreach($posts as $post)
+        <h3><strong>TITOLO: </strong>{{$post->title}}</h3>
+        <p><strong>BODY: </strong>{{$post->body}}</p>
+        <p><strong>CREATED: </strong>{{$post->created_at}}</p>
+        <p><strong>UPDATED: </strong>{{$post->updated_at}}</p>
+    @endforeach
+</div>
