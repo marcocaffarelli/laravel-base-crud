408eed3f390920ae71bc09e3e82975f018b09aef
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-20 13:59:08 +0100

    Creato il file _show.scss
    Creato il file _show.scss

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.head')
 <body>
-    <div>
-        <h1>POST {{$post->id}}</h1>
-        <h3><strong>TITOLO: </strong>{{$post->title}}</h3>
-        <p><strong>BODY: </strong>{{$post->body}}</p>
-        <p><strong>CREATED: </strong>{{$post->created_at}}</p>
-        <p><strong>UPDATED: </strong>{{$post->updated_at}}</p>
+    <div class="show">
+        <div>
+            <h1>POST {{$post->id}}</h1>
+            <h3><strong>TITOLO: </strong>{{$post->title}}</h3>
+            <p><strong>BODY: </strong>{{$post->body}}</p>
+            <p><strong>CREATED: </strong>{{$post->created_at}}</p>
+            <p><strong>UPDATED: </strong>{{$post->updated_at}}</p>
+        </div>
+        <a href="{{ route('blog') }}" class="btn">RETURN</a>
     </div>
-    <a href="{{ route('blog') }}" style="background-color: darkblue; color: white; padding: 3px; margin: 5px; border: 1px solid black; border-radius: 5px;">RETURN</a>
 </body>
